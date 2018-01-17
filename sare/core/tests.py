553961aecc0e695c842e72faf10771f6474ee725
Samuel Barbosa <samuka1@gmail.com>
2018-01-17 17:33:27 -0300

    Incluindo home (landing page) e seus respectivos testes
    close #4

--- sare/core/tests.py
+++ sare/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+
+class HomeTest(TestCase):
+    def test_get(self):
+        """GET / deve retornar status code 200"""
+        response = self.client.get('/')
+        self.assertEqual(200, response.status_code)
+
+    def test_template(self):
+        """Must use index.html"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
