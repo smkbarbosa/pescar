496b9cd2ec225714ce8507cc5adcfba116842794
Samuel Barbosa <samuka1@gmail.com>
2018-01-27 01:40:32 -0300

    remove django-toolbar

--- sare/urls.py
+++ sare/urls.py
@@ -34,9 +34,3 @@ urlpatterns = [
                                                         ])),
     url(r'^admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
