fdb905ceca815cc873e2edeafe56feddd6eb7b6f
Samuel Barbosa <samuka1@gmail.com>
2018-02-23 11:59:40 -0300

    ajusta permissão somente leitura para não admin

--- sare/questionarios/admin.py
+++ sare/questionarios/admin.py
@@ -5,15 +5,23 @@ from sare.questionarios.models import Questionario
 
 
 class QuestionarioModelAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'email', 'cpf', 'criado_em', 'preenchido_hoje')
+    list_display = ('nome', 'email', 'cpf', 'curso', 'criado_em', 'preenchido_hoje')
     date_hierarchy = 'criado_em'
-    search_fields = ('nome', 'email', 'cpf', 'criado_em')
-    list_filter = ('criado_em',)
+    search_fields = ('nome', 'email', 'cpf', 'curso','criado_em')
+    list_filter = ('criado_em', 'curso')
 
     def preenchido_hoje(self, obj):
-        return obj.criado_em == now().date()
+        return obj.criado_em.date() == now().date()
 
     preenchido_hoje.short_description = 'respondeu hoje?'
     preenchido_hoje.boolean = True
+
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            return [f.name for f in self.model._meta.fields]
+        return super(QuestionarioModelAdmin, self).get_readonly_fields(
+                request, obj=obj
+        )
+
 
 admin.site.register(Questionario, QuestionarioModelAdmin)
