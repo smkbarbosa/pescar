70736f13a105ac2c42509c90d2cd7fa436e80beb
Samuel Barbosa <contato@smkbarbosa.eti.br>
2018-03-13 20:18:37 -0300

    desabilita form

--- sare/questionarios/tests/test_view_new.py
+++ sare/questionarios/tests/test_view_new.py
@@ -14,7 +14,7 @@ class QuestionarioNovoGet(TestCase):
     def setUp(self):
         self.resp = self.client.get(r('questionarios:new'))
 
-    # @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
     def test_get(self):
         """GET /questionario/ deve retornar status code 200"""
         self.assertEqual(200, self.resp.status_code)
@@ -22,7 +22,7 @@ class QuestionarioNovoGet(TestCase):
     # def test_template(self):
     #     """Deve utilizar o template form_socioeconomico.html"""
     #     self.assertTemplateUsed(self.resp, 'questionarios/form_socioeconomico.html')
-    # @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
     def test_html(self):
         """HTML deve conter algumas tags específicas"""
         tags = (('<form', 1),
@@ -32,12 +32,12 @@ class QuestionarioNovoGet(TestCase):
             with self.subTest():
                 self.assertContains(self.resp, text, count)
 
-    # @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
     def test_csrf(self):
         """Html deve conter csrf"""
         self.assertContains(self.resp, 'csrfmiddlewaretoken')
 
-    # @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
     def test_has_form(self):
         """Context deve ter form de questionario"""
         form = self.resp.context['form']
@@ -60,16 +60,16 @@ class QuestionarioNovoPost(TestCase):
         self.uuid = Questionario.objects.first().hashId
         self.email = mail.outbox[0]
 
-    # @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
     def test_post(self):
         """POST valid deve redirecionar to /questionario/00000000-0000-0000-0000-000000000000/"""
         self.assertRedirects(self.resp, r('questionarios:detalhe', self.uuid))
 
-    # @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
     def test_envia_email_questionario(self):
         self.assertEqual(1, len(mail.outbox))
 
-    # @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
     def test_salva_questionario(self):
         self.assertTrue(Questionario.objects.exists())
 
@@ -78,28 +78,28 @@ class QuestionarioNovoPostInvalid(TestCase):
     def setUp(self):
         self.resp = self.client.post(r('questionarios:new'), {})  # data é um dicionário vazio
 
-   # @skipIf('ERROR','Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR','Pulando enquanto o redirecionamento for para consultar')
     def test_post(self):
         """Post Invalid não deve redirecionar"""
         self.assertEqual(200, self.resp.status_code)
 
-    # @skipIf('ERROR','Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR','Pulando enquanto o redirecionamento for para consultar')
     def test_template(self):
         self.assertTemplateUsed(self.resp, 'questionarios/form_socioeconomico.html')
         self.assertTemplateUsed(self.resp, 'material/includes/material_css.html')
         self.assertTemplateUsed(self.resp, 'material/includes/material_js.html')
         self.assertTemplateUsed(self.resp, 'material/form.html')
 
-    # @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
     def test_has_form(self):
         form = self.resp.context['form']
         self.assertIsInstance(form, QuestionarioForm)
 
-    # @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
     def test_form_has_errors(self):
         form = self.resp.context['form']
         self.assertTrue(form.errors)
 
-    # @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
+    @skipIf('ERROR', 'Pulando enquanto o redirecionamento for para consultar')
     def test_nao_salva_questionario(self):
         self.assertFalse(Questionario.objects.exists())
