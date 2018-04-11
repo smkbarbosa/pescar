3cb9a00a85be0a6ac149c0ad619cd2f98718d695
Samuel Barbosa <contato@smkbarbosa.eti.br>
2018-04-11 13:10:22 -0300

    adiciona forms separados

--- sare/core/forms.py
+++ sare/core/forms.py
@@ -3,21 +3,18 @@ from django.forms import Select, RadioSelect
 from material import *
 
 
-from sare.core.models import Aluno
+from sare.core.models import Aluno, Endereco
 
 
 class AlunoForm(forms.ModelForm):
 
     title = 'Formulário Socioeconômico-Cultural'
 
-
     layout = Layout(
         Fieldset("Dados Pessoais"),
         Row(Span4('nome'), Span4('email'),
             Span2('cpf'), Span2('fone')),
-        Row(Span4('sexo'), Span3('cep'),
-            Span5('endereco')),
-        Row(Span3('num_casa'), Span3('bairro'), Span3('cidade'), Span3('estado')),
+        Row(Span4('sexo')),
 
         Fieldset('Dados acadêmicos'),
         Field('curso'),
@@ -35,4 +32,18 @@ class AlunoForm(forms.ModelForm):
             'cpf': 'Somente números',
         }
 
+
+class EnderecoForm(forms.ModelForm):
+
+    layout = Layout(
+        Fieldset('Endereço:'),
+        Row(Span3('cep'),
+            Span5('endereco')),
+        Row(Span3('num_casa'), Span3('bairro'), Span3('cidade'), Span3('estado')
+            )
+    )
+
+    class Meta:
+        model = Endereco
+        fields = '__all__'
 
